ecf13caa9cfae93f172bc9311475b17fa6889dc2
Verenicin <email@gmail.com>
2025-09-24 09:40:27 +0300

    ...

--- groupproject/groupproject.cpp
+++ groupproject/groupproject.cpp
@@ -6,5 +6,26 @@ using namespace std;
 
 int main()
 {
-    
+    int stag;
+    cout << "Введите год с которого начинат выводить список книг: ";
+    cin >> stag;
+
+    // Текущий год
+    int tekYear = 2024;
+
+    // Вывод фамилий работников, стаж которых превышает введенное значение
+    bool found = false;
+    for (int i = 0; i < SIZE; ++i) {
+        int workStag = tekYear - workers[i].year;
+        if (workStag > stag) {
+            cout << "Список книг: " << workers[i].name << "Автор:" << workers[i].autor << endl;
+            found = true;
+        }
+    }
+
+    if (!found) {
+        cout << "Нет книг такого года" << endl;
+    }
+
+    return 0;
 }
